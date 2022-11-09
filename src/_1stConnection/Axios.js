7cb744f880dd06b08e2afafdfc31525e8ed4e4ee
Ait 79i <abdelkarimaithqi@gmail.com>
2022-11-10 00:31:59 +0100

    the second methode "async await"to get data from API

--- src/_1stConnection/Axios.js
+++ src/_1stConnection/Axios.js
@@ -1,11 +1,45 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+// const Axios = () => { //! M1
+// 	const [users, setUsers] = useState([]);
+// 	useEffect(() => {
+// 		axios.get("https://jsonplaceholder.typicode.com/users").then((res) => {
+// 			setUsers(res.data);
+// 			console.log(res.data);
+// 		});
+// 	}, []);
+// 	return (
+// 		<div>
+// 			<h1>Users Number : {users.length}</h1>
+// 			{users.map((user) => {
+// 				return (
+// 					<div className="child" key={user.id}>
+// 						<h3 style={{ color: "rgb(92,63,3)" }}>
+// 							{user.name}
+// 							{user.username}
+// 						</h3>
+// 						<p>email:{user.email}</p>
+// 						<p>
+// 							ville:{user.address.city} rue:{user.address.street}
+// 						</p>
+// 					</div>
+// 				);
+// 			})}
+// 		</div>
+// 	);
+// };
+
 const Axios = () => {
+	//! M2
 	const [users, setUsers] = useState([]);
 	useEffect(() => {
-		axios
-			.get("https://jsonplaceholder.typicode.com/users")
-      .then((res) => { setUsers(res.data);console.log(res.data); });
+		const getData = async () => {
+			const utilisateurs = await axios.get(
+				"https://jsonplaceholder.typicode.com/users"
+			);
+			setUsers(utilisateurs.data);
+		};
+		getData();
 	}, []);
 	return (
 		<div>
@@ -27,5 +61,4 @@ const Axios = () => {
 		</div>
 	);
 };
-
 export default Axios;
