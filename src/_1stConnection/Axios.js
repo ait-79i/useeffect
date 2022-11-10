1de649edb7da1308a1c0b27aa7c157d5d8ddcd01
Ait 79i <abdelkarimaithqi@gmail.com>
2022-11-10 01:05:59 +0100

    last modification

--- src/_1stConnection/Axios.js
+++ src/_1stConnection/Axios.js
@@ -1,39 +1,17 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 
-// const Axios = () => { //! M1
-// 	const [users, setUsers] = useState([]);
-// 	useEffect(() => {
-// 		axios.get("https://jsonplaceholder.typicode.com/users").then((res) => {
-// 			setUsers(res.data);
-// 			console.log(res.data);
-// 		});
-// 	}, []);
-// 	return (
-// 		<div>
-// 			<h1>Users Number : {users.length}</h1>
-// 			{users.map((user) => {
-// 				return (
-// 					<div className="child" key={user.id}>
-// 						<h3 style={{ color: "rgb(92,63,3)" }}>
-// 							{user.name}
-// 							{user.username}
-// 						</h3>
-// 						<p>email:{user.email}</p>
-// 						<p>
-// 							ville:{user.address.city} rue:{user.address.street}
-// 						</p>
-// 					</div>
-// 				);
-// 			})}
-// 		</div>
-// 	);
-// };
-
 const Axios = () => {
-	//! M2
 	const [users, setUsers] = useState([]);
+	// useEffect(() => {  //! M1
+	// 	axios.get("https://jsonplaceholder.typicode.com/users").then((res) => {
+	// 		setUsers(res.data);
+	// 		console.log(res.data);
+	// 	});
+	// }, []);
+
 	useEffect(() => {
+		//! M1 ASYNC-AWAIT
 		const getData = async () => {
 			const utilisateurs = await axios.get(
 				"https://jsonplaceholder.typicode.com/users"
@@ -62,4 +40,5 @@ const Axios = () => {
 		</div>
 	);
 };
+
 export default Axios;
--- src/_1stConnection/Fetch.jsx
+++ src/_1stConnection/Fetch.jsx
@@ -1,6 +1,11 @@
-import {useEffect, useState} from 'react'
+import { useEffect, useState } from 'react'
 
 function Fetch() {
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+
+    fetch('https://jsonplaceholder.typicode.com/users').then((res) => { return res.json() }).then((utilisateurs) => { setUsers(utilisateurs) })
+  })
   return (
     <div>Fetch</div>
   )
